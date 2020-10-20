7c9eb01a6a351f829daefecdd66c30efa10c5491
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-10-20 19:00:19 -0200

    Initial commit

---
+++ altera_rodape.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name: altera rodape
+Plugin URI: www.rodape.com
+Description: altera informações do rodape
+Version: 1.0
+Author: Alexandre
+Author URI: alexandre.com
+License: GLP2
+Text Domain: altera informações do rodape
+*/
+ 
